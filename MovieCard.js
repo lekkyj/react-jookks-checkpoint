f2310ad2d965759be892020348a123fd5b6a1fbe
lekkyj <lekanjayeola0@gmail.com>
2025-05-14 09:10:26 +0100

    Create MovieCard.js

---
+++ MovieCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <img src={movie.posterURL} alt={movie.title} />
+      <h2>{movie.title}</h2>
+      <p>{movie.description}</p>
+      <p>⭐ {movie.rating}</p>
+    </div>
+  );
+};
+
+export default MovieCard;
